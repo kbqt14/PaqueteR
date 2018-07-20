7df6dbd82ec49189458fd13f1e23ba735655c54f
kbqt14 <kbqt14@gmail.com>
2018-07-20 13:12:07 -0300

    qwer

---
+++ R/funcion2.r
@@ -0,0 +1,16 @@
+#' Reparacion del set de datos de clorofila
+#'
+#' @param NumerDeGatos el numero de gatos
+#'
+#' @return none
+#' @export
+#'
+#' @examples
+#'
+#' CrearGatos(10)
+#'
+CrearGato <- function(numeroDeGatos){
+
+for (i in 1:numero de gatos)
+}
+require(devtools)
